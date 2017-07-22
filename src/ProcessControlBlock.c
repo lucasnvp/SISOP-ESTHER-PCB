ad9c392ed1511395ab79d255b21af6d044941a06
Lucas Visser <lucasnvp@gmail.com>
2017-07-22 19:31:42 -0300

    Nuevo PCB. 	- Se cambiaron los queue por lists. 	-Se agrego el Quantum.

--- src/ProcessControlBlock.c
+++ src/ProcessControlBlock.c
@@ -18,26 +18,28 @@ int main(void) {
 	t_metadata_program* metadata = metadata_desde_literal(programa);
 
 	//Creo el PCB
-	PCB_t* newPCB = PCB_new_pointer(1, 4, metadata);
+	PCB_t* newPCB = PCB_new_pointer(1, 4, metadata, 0);
 
 	//Variable
 	VARIABLE_T * variableA = variable_new('a',0,0,4);
 	VARIABLE_T * variableG = variable_new('g',0,4,4);
 
-	t_queue* queue_vars = queue_create();
-	queue_push(queue_vars, variableA);
-	queue_push(queue_vars, variableG);
-
 	//Linea 0 SP
-	STACKPOINTER_T * lineSP = stack_new(NULL, queue_vars, NULL, NULL);
-	push_stack(newPCB, lineSP);
+	t_list* list_vars = list_create();
+	list_add(list_vars, variableA);
+	list_add(list_vars, variableG);
+
+	STACKPOINTER_T * lineSP = stack_new(NULL,list_vars,NULL,NULL);
+	list_add(newPCB->StackPointer, lineSP);
 
 	//Linea 1 SP
-	t_queue* queue_args = queue_create();
 	VARIABLE_T * variableACopy = variable_new('a',0,8,4);
-	queue_push(queue_args, variableACopy);
-	STACKPOINTER_T * lineSP2 = stack_new(queue_args, NULL, 3, variableG);
-	push_stack(newPCB, lineSP2);
+
+	t_list* list_args = list_create();
+	list_add(list_args, variableACopy);
+
+	STACKPOINTER_T * lineSP2 = stack_new(list_args,NULL,3,variableG);
+	list_add(newPCB->StackPointer, lineSP2);
 
 	//Muestro el PID Del proceso
 	print_PCB(newPCB);
